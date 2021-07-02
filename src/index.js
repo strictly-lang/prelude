0c04555a6607b88e8a2f99c38209c1ec2be5e823
plusgut <github@webentwickler2-0.de>
2021-07-02 21:14:00 +0200

    feat(fetchText): add fetch-text

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 export function zipWithIndex(list) {
   return list.map((entity, index) => [entity, index]);
 }
+
+export function* fetchText(url) {
+  yield {
+    _type: "Pending",
+  };
+
+  return fetch(url)
+    .then((response) => response.text())
+    .then((response) => ({ _type: "Ok", [0]: response }))
+    .catch((error) => ({ _type: "Error", [0]: error }));
+}
